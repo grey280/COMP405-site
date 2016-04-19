a0e95dc8a67533715ac81bcd906869568a448a58
Grey Patterson <greypatterson@gmail.com>
2016-04-18 19:46:11 -0700

    Add more syllabi

--- work.php
+++ work.php
@@ -32,19 +32,19 @@
       <div class="row">
         <div class="col-md-3">
           <h2>Transfer Credits</h2>
-          <table class="table table-bordered table-striped">
+          <table class="table table-bordered">
             <tr><td class="">IB*HSTEUR: History Europe ME</td></tr>
             <tr><td class="">IB*PHYSICS: Physics</td></tr>
           </table>
         </div>
         <div class="col-md-3">
           <h2>Fall Semester</h2>
-          <table class="table table-bordered table-striped">
+          <table class="table table-bordered">
             <tr><td class="success"><a href="./classes/freshman/fall/COMP121.php">COMP121: Web Design</a></td></tr>
             <tr><td class="warning"><a download href="./classes/freshman/fall/COMP160.pdf">COMP160: Functions</a></td></tr>
-            <tr><td class="">MATH170: Calculus I</td></tr>
+            <tr><td class="warning"><a download href="./classes/freshman/fall/MATH170.pdf">MATH170: Calculus I</a></td></tr>
             <tr><td class="warning"><a download href="./classes/freshman/fall/IDST007.pdf">IDST007: Colloquium</a></td></tr>
-            <tr><td class="">MUSC117: Concert Choir</td></tr>
+            <tr><td class="warning"><a download href="./classes/freshman/fall/MUSC117.pdf">MUSC117: Concert Choir</a></td></tr>
             <tr><td class="warning"><a download href="./classes/freshman/fall/MUSC121.pdf">MUSC121: Music Theory I</a></td></tr>
             <tr><td class="warning"><a download href="./classes/freshman/fall/MUSC122.pdf">MUSC122: ETSS I</a></td></tr>
             <tr><td class="">MUSC203: Applied Voice: Major</td></tr>
@@ -52,18 +52,18 @@
         </div>
         <div class="col-md-3">
           <h2>January Term</h2>
-          <table class="table table-bordered table-striped">
+          <table class="table table-bordered">
             <tr><td class="">MATH220: Intro to Proofs</td></tr>
           </table>
         </div>
         <div class="col-md-3">
           <h2>Spring Semester</h2>
-          <table class="table table-bordered table-striped">
+          <table class="table table-bordered">
             <tr><td class="">COMP161: Data Structures</td></tr>
             <tr><td class="warning"><a download href="./classes/freshman/spring/INQS125.pdf">INQS125: Inquiry Seminar</a></td></tr>
             <tr><td class="">MUSC108: Men's Glee</td></tr>
             <tr><td class="">MUSC117: Concert Choir</td></tr>
-            <tr><td class="">MUSC123: Music Theory II</td></tr>
+            <tr><td class="warning"><a download href="./classes/freshman/spring/MUSC123.pdf">MUSC123: Music Theory II</a></td></tr>
             <tr><td class="">MUSC124: ETSS II</td></tr>
             <tr><td class="warning"><a download href="./classes/freshman/spring/MUSC135.pdf">MUSC135: Understanding Jazz</a></td></tr>
             <tr><td class="">MUSC203: Applied Voice: Major</td></tr>
@@ -80,34 +80,34 @@
       <div class="row">
         <div class="col-md-3">
           <h2>Summer Term</h2>
-          <table class="table table-bordered table-striped">
+          <table class="table table-bordered">
             <tr><td class="">COMP260: Database Management Systems</td></tr>
           </table>
         </div>
         <div class="col-md-3">
           <h2>Fall Semester</h2>
-          <table class="table table-bordered table-striped">
+          <table class="table table-bordered">
             <tr><td class="">COMP262: Data Abstraction</td></tr>
-            <tr><td class="">MATH230: Discrete Mathematics</td></tr>
-            <tr><td class="warning"><a download href="./classes/sophomore/fall/MUSC108.pdf">MUSC108: Men's Glee</td></tr>
+            <tr><td class="warning"><a download href="./classes/sophomore/fall/MATH230.pdf">MATH230: Discrete Mathematics</a></td></tr>
+            <tr><td class="warning"><a download href="./classes/sophomore/fall/MUSC108.pdf">MUSC108: Men's Glee</a></td></tr>
             <tr><td class="warning"><a download href="./classes/sophomore/fall/MUSC117.pdf">MUSC117: Concert Choir</a></td></tr>
             <tr><td class="">MUSC203*3: Applied Composition: Major</td></tr>
             <tr><td class="">MUSC203*10: Applied Voice: Major</td></tr>
-            <tr><td class="">MUSC221: Music Theory III</td></tr>
-            <tr><td class="">MUSC222: ETSS III</td></tr>
-            <tr><td class="">MUSC253: Music Cultures of the World</td></tr>
+            <tr><td class="warning"><a download href="./classes/sophomore/fall/MUSC221.pdf">MUSC221: Music Theory III</a></td></tr>
+            <tr><td class="warning"><a download href="./classes/sophomore/fall/MUSC222.pdf">MUSC222: ETSS III</a></td></tr>
+            <tr><td class="warning"><a download href="./classes/sophomore/fall/MUSC253.pdf">MUSC253: Music Cultures of the World</a></td></tr>
           </table>
         </div>
         <div class="col-md-3">
           <h2>January Term</h2>
-          <table class="table table-bordered table-striped">
+          <table class="table table-bordered">
             <tr><td class="">COMP330: Operating Systems/Networking</td></tr>
             <tr><td class="warning"><a download href="./classes/sophomore/january/ENGL304.pdf">ENGL304: Environmental Literature</a></td></tr>
           </table>
         </div>
         <div class="col-md-3">
           <h2>Spring Semester</h2>
-          <table class="table table-bordered table-striped">
+          <table class="table table-bordered">
             <tr><td class="warning"><a download href="./classes/sophomore/spring/COMP377.pdf">COMP377: Computer Architecture</a></td></tr>
             <tr><td class="warning"><!-- TODO: Build a 'coursework' page for this --><a download href="./classes/sophomore/spring/COMP405.pdf">COMP405: Platform-Based Software Development</a></td></tr>
             <tr><td class="">HHPA067: Scuba</td></tr>
@@ -129,13 +129,13 @@
       <div class="row">
         <div class="col-md-3">
           <h2>Summer</h2>
-          <table class="table table-bordered table-striped">
+          <table class="table table-bordered">
             <tr><td class="">COMP487: Research/Internship</td></tr>
           </table>
         </div>
         <div class="col-md-3">
           <h2>Fall Semester</h2>
-          <table class="table table-bordered table-striped">
+          <table class="table table-bordered">
             <tr><td class="">HIST365: Ethnic Diversity/Eastern Europe</td></tr>
             <tr><td class="">IDST031: Intercultural Communication: Departure/Reentry</td></tr>
             <tr><td class="">MLGR101: Elementary German 1</td></tr>
@@ -159,7 +159,7 @@
       <div class="row" id="key">
         <div class="col-md-12">
           <h1>Key</h1>
-          <table class="table table-bordered table-striped">
+          <table class="table table-bordered">
             <tr>
               <td>No Attachments</td>
               <td class="warning">Syllabus Available</td>
